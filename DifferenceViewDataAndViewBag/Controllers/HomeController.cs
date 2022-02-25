9552fee96d7762f498ca919d4b1d0229418f337b
Amit <amitkr24031995@gmail.com>
2022-02-25 07:49:16 +0530

    Adding Difference between viewData and viewBag

---
+++ DifferenceViewDataAndViewBag/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using DifferenceViewDataAndViewBag.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DifferenceViewDataAndViewBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Message1"] = "Data comes from ViewData";
+            ViewBag.Message2 = "Data comes from ViewBag";
+
+            ViewData["CurrentDate1"] = DateTime.Now.ToString();
+            ViewBag.CurrentDate2 = DateTime.Now.ToString();
+
+            string[] games = { "Hockey", "Football", "Cricket", "Baseball" };
+            ViewData["GamesArray1"] = games;
+            ViewBag.GamesArray2 = games;
+
+            Employee obj = new Employee();
+            obj.EmpId = 11;
+            obj.EmpName = "Amit";
+            obj.EmpAge = 23;
+
+            ViewData["Employee1"] = obj;
+            ViewBag.Employee2 = obj;
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ DifferenceViewDataAndViewBag/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DifferenceViewDataAndViewBag.MvcApplication" Language="C#" %>
